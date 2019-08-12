619fca633d15141a10d15da40e99852da48caa53
huo376230434 <415384303@qq.com>
2019-08-12 21:47:38 +0800

    Update xampp_centos.sh

--- sh/xampp_centos.sh
+++ sh/xampp_centos.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 yum update -y
-wget https://liquidtelecom.dl.sourceforge.net/project/xampp/XAMPP%20Linux/7.2.21/xampp-linux-x64-7.2.21-0-installer.run
+#wget https://liquidtelecom.dl.sourceforge.net/project/xampp/XAMPP%20Linux/7.2.21/xampp-linux-x64-7.2.21-0-installer.run
 chmod -R 777 ./xampp-linux-x64-7.2.21-0-installer.run
 echo '安装xampp';
+（
 ./xampp-linux-x64-7.2.21-0-installer.run
+）
 echo '安装xampp完成';
 echo "开机自启动";
 ln -s /opt/lampp/xampp /etc/rc.d/init.d/xampp
