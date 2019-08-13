dd8301537a3646e588aa659539643b09e9c31aee
huo <376230434@qq.com>
2019-08-13 14:30:55 +0800

    自动提交

--- sh/test.sh
+++ sh/test.sh
@@ -1,46 +1,12 @@
 #!/bin/sh
-
-function error() {
-    echo  "\033[31m $1 \033[0m"
-}
-
-function success() {
-    echo  "\033[32m $1 \033[0m"
-}
-
-function info() {
-    echo  "\033[37m $1 \033[0m"
-}
-
-function warn() {
-    echo  "\033[33m $1 \033[0m"
-}
-
-#字符串是否在文件之内
-function is_str_in_file() {
-    str=$1;
-    file=$2;
-    #echo $str;
-    res=`grep "$str" $file  `
-    count=${#res}
-    if  [ $count -gt 0 ];then
-   return 1;
-    else
-return 0;
-    fi
-}
-
-#如果字符串不存在文件中，则追加
-function append_file_if_not_exists() {
-   is_str_in_file "$1" $2
-   res=$?;
-if [ $res -eq 0 ] ; then
-   echo $1 >> $2;
+util_base_url=https://github.com/huo376230434/daily-utils/blob/master
+if [ ! -e ./base.sh ]; then
+wget $util_base_url/sh/base.sh
 fi
-}
+source ./base.sh;
 
 #调用字符串不存在方法的demo
-#str="export PATH=\"/opt/lampp/bin:\$PATH\""
-#file=$PWD/profile
-#append_file_if_not_exists "$str" $file
+str="export PATH=\"/opt/lampp/bin:\$PATH\""
+file=$PWD/profile
+append_file_if_not_exists "$str" $file
 
