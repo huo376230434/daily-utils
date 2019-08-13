a7e9e521ef8ef8e28a1e88bcb27caa1ae3c06896
huo <376230434@qq.com>
2019-08-13 15:46:59 +0800

    自动提交

--- sh/common/base.sh
+++ sh/common/base.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 function error() {
-    echo  "\033[31m $1 \033[0m"
+    echo  -e "\033[31m $1 \033[0m"
 }
 
 function success() {
-    echo  "\033[32m $1 \033[0m"
+    echo  -e "\033[32m $1 \033[0m"
 }
 
 function info() {
-    echo  "\033[37m $1 \033[0m"
+    echo  -e "\033[37m $1 \033[0m"
 }
 
 function warn() {
-    echo  "\033[33m $1 \033[0m"
+    echo  -e "\033[33m $1 \033[0m"
 }
 
 
