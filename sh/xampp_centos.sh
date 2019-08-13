c94c6d8685bf1fc96480887592049a8416c36f73
huo376230434 <415384303@qq.com>
2019-08-13 10:20:50 +0800

    Update xampp_centos.sh

--- sh/xampp_centos.sh
+++ sh/xampp_centos.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 #yum update -y
+if [ ! $1 ]
+then 
+echo '数据库密码必填 调用格式为 ./xampp_centos.sh 数据库密码 是否已经拉取过xamppintaller(默认为false 已经拉取过可传 1 )'
+exit 0;
+fi
+
 if [ ! $2 ]
+echo '拉取xampp installer'
 wget https://liquidtelecom.dl.sourceforge.net/project/xampp/XAMPP%20Linux/7.2.21/xampp-linux-x64-7.2.21-0-installer.run
 then 
 echo 'xampp installer 已经存在，直接用'
